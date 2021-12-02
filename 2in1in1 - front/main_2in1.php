f180758c6e7d1bafb34149e6ed51d4452991a891
Mavs <88785183+Herm88@users.noreply.github.com>
2021-12-02 10:17:56 +0800

    front end

---
+++ 2in1in1 - front/main_2in1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel = "stylesheet" href = "style.css">
+<title>2 in 1 in 1</title>
+</head>
+<body>
+<div>
+	<table style = "width:100%" >
+		<tr>
+			<th> <a href = "main_2in1.html" ><h3>Home</h3></a></th>
+			<th> <a href = "Update.html" ><h3>Update</h3></a></th>
+			<th> <a href = "Contact.html" ><h3>Contact</h3></a></th>
+		</tr>	
+	</table>
+	
+	<h4 text-align: "center" >MULTIPURPOSE</h4>
+	<h4 text-align: "center" >INVENTORY  SYSTEM</h4>
+	
+	
+	<table style = "width:100%" >
+		<tr>
+			<th><br>
+			<label for="user">Owner's Name </label><br><input type="text" placeholder="ownersName"> <br><br>
+            Products Name eg.(CAR,AIRCON,ETC)<br> <input type="text" name="productName"placeholder = "All item you bought"> <br><br>
+			Quantity((CAR(1)), (AIRCON(2)))<br> <input type="text" name="quantity"placeholder = "Number of products" ><br><br>
+            Total Price ((CAR(1) 1000,000),(AIRCON(2)100,000),(TOTAL PRICE))<br><input type="name" name="price"><br><br>
+            <button >Save</button></th>
+		</tr>	
+	</table>
+	<img src="companyLogo.jpg" width="100%" height="190" style="margin-top:5px; margin-bottom: 0px"  style="border-radius: 60px" style="display: flex;">
+	
+</div>
+</body>
+</html>
