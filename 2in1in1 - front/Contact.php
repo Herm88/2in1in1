de846f60b60c59bd6645a98a31bf504fedc73787
Mavs <88785183+Herm88@users.noreply.github.com>
2021-12-02 10:21:00 +0800

    front end added by Mavs

---
+++ 2in1in1 - front/Contact.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel = "stylesheet" href = "style.css">
+<title>2 in 1 in 1</title>
+</head>
+<body>
+<div>
+	<table style = "width:100%" >
+		<tr>
+			<th> <a href = "main_2in1.html" ><h3>Home</h3></a></th>
+			<th> <a href = "Update.html" ><h3>Update</h3></a></th>
+			<th> <a href = "Contact.html" ><h3>Contact</h3></a></th>
+		</tr>	
+	</table>
+	
+	<h4>Developers</h4>
+	
+	<table style = "width:100%" >
+		<tr>
+			<th> <img src="Vinz.png" width="200" height="200" style="margin-top:50px; margin-bottom: 0px"  style="border-radius: 60px" style="display: flex;"></th>
+			<th> <img src="rica.jpg" width="200" height="200" style="margin-top:50px; margin-bottom: 0px"  style="border-radius: 60px" style="display: flex;"></th>
+			<th> <img src="Mavs.jpg" width="200" height="200" style="margin-top:50px; margin-bottom: 0px"  style="border-radius: 60px" style="display: flex;"></th>
+		</tr>
+		<tr>
+		
+			<th> <a href = "https://m.me/bayonz" ><h3>Vinz Abayon</h3></a></th>
+			<th> <a href = "https://m.me/ricamae.besco.12" ><h3>Rica Mae Besco</h3></a></th>
+			<th> <a href = "https://m.me/mavnco" ><h3>Mavs Francisco</h3></a></th>
+		</tr>
+	<tr>
+		
+			<th> <a href = "https://m.me/bayonz" ><img src="messenger.png" width="50" height="50" style="margin-top:0px; margin-bottom: 20px"  style="border-radius: 60px" style="display: flex;"></th></a></th>
+			<th> <a href = "https://m.me/ricamae.besco.12" ><img src="messenger.png" width="50" height="50" style="margin-top:0px; margin-bottom: 20px"  style="border-radius: 60px" style="display: flex;"></th></a></th>
+			<th> <a href = "https://m.me/mavnco" ><img src="messenger.png" width="50" height="50" style="margin-top:0px; margin-bottom: 20px"  style="border-radius: 60px" style="display: flex;"></th></a></th>
+		</tr>			
+	</table>
+	<img src="companyLogo.jpg" width="100%" height="190" style="margin-top:5px; margin-bottom: 0px"  style="border-radius: 60px" style="display: flex;">
+	
+</div>
+</body>
+</html>
